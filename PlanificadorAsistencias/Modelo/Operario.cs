915f5db0b1f0a1f441bb7a07e82bb9f925d3e5ff
PabloRuizz <pabloruiz6@gmail.com>
2025-05-13 00:55:53 +0200

    Estructura

---
+++ PlanificadorAsistencias/Modelo/Operario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Operario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Disponible { get; set; }
+        public List<string> Habilidades { get; set; } = new List<string>();
+    }
+}
